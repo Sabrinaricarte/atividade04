542c2e1c9326dba714416b24e6d7c052a923ed78
Sabrinaricarte <113310723+Sabrinaricarte@users.noreply.github.com>
2022-10-05 16:51:30 -0300

    Add files via upload

---
+++ sabrinaatv.py
@@ -0,0 +1,12 @@
+Soma: int
+x: int
+x = int (input("digite um numero(0- sair):"))
+soma = 0
+
+while x != 8:
+ soma = soma + x
+x = int(input("digite um numero(0- sair):"))
+
+print("soma vale:", soma)
+
+
